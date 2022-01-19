d296871444d28c0bddc2286538aeb87521ea85cf
Chun-Hao To <chto@pitzer-rw01.ten.osc.edu>
2022-01-19 15:50:29 -0500

    modify requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = ["numpy", "torch>=1.10.1", "mpi4py", "schwimmbad", "emcee>=3.0.2", "h5py", "pydoe2", "scipy"]
 
 test_requirements = ['pytest>=3', ]
 
