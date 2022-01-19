ed528f7c54a988350a678b9a7421707b138b3431
CHUN-HAO TO <chto@stanford.edu>
2022-01-19 13:09:04 -0800

    add train_gpu

--- linna/train_gpu.py
+++ linna/train_gpu.py
@@ -3,36 +3,22 @@ import pyDOE2
 import sample_generator as sg
 from copy import deepcopy
 import os
-import sys
-path = "/home/users/chto/code/lighthouse/analysis/"
-sys.path.append(path+"/../python/")
-sys.path.append(path+"/../python/datavector/")
-sys.path.append(path+"/../lib/")
-sys.path.append(path+"/../python/nnacc/nnacc/")
-from predictor import * 
-import util_chto
-import generate_data_vector
-import  generate_batch_theory_datavector
-from schwimmbad import MPIPool
+from .predictor_gpu import * 
 import glob
 import torch
 from torch.utils.data import Dataset, DataLoader
 import pickle
-import predictor
-import sampler
+import .sampler
 import sys
-import emcee
 from sklearn.cluster import MeanShift, estimate_bandwidth, KMeans
-from nn import *
+from .nn import *
 from scipy.special import erf
 from scipy.stats import chi2
 import io
 from torch.utils import mkldnn as mkldnn_utils
 import mpi4py.rc
 mpi4py.rc.initialize = False
-import itnn_accelerator_batch_v2_gpu
-import torch
-print(path)
+import .itnn_accelerator_batch_v2_gpu
 
 if __name__=="__main__":
     while(not torch.cuda.is_available()):
