f0832b0c11177a1fe677331bb5475345733658c8
CHUN-HAO TO <chto@stanford.edu>
2022-01-19 18:16:11 -0800

    test

---
+++ tests/test_main.py
@@ -0,0 +1,58 @@
+import pytest
+import numpy as np
+import matplotlib.pyplot as plt 
+from linna.main import ml_sampler
+from linna.util import *
+from linna.nn import *
+nit =  np.random.uniform(size=ndim)
+#mean value 
+means = np.array([0.1, 1])
+
+ndim = len(init)
+#covariance matrix 
+cov = np.diag([0.5, 0.2])
+#Prior, Theory, and likelihood
+ndim = 2
+init =  np.random.uniform(size=ndim)
+#mean value 
+means = np.array([0.1, 1])
+
+ndim = len(init)
+#covariance matrix 
+cov = np.diag([0.5, 0.2])
+#Prior, Theory, and likelihood
+
+priors = []
+for i in range(ndim):
+    priors.append({
+        'param': 'test_{0}'.format(i),
+        'dist': 'flat',
+        'arg1': -2.,
+        'arg2': 2.
+    })
+
+def theory(x, outdirs):
+    x_new = deepcopy(x[1])
+    return x_new
+nwalkers = 4 #Number of mcmc walker
+pool = None
+ntrainArr = [1]
+nvalArr = [1]
+nkeepArr = [2]
+ntimesArr = [1]
+ntautolArr = [0.1]
+temperatureArr =  [2.0]
+params = {}
+params["trainingoption"] = 1
+params["num_epochs"] = 10
+params["batch_size"] = 5
+ypositive=False
+dolog10index = None
+outdir = os.path.abspath(os.getcwd())+"/out/2dgaussian_Fulltconn/"
+gpunode = None
+def testmain():
+    chain, logprob = ml_sampler_core(ntrainArr, nvalArr, nkeepArr, ntimesArr, ntautolArr, outdir, theory, priors, means, cov,  init, pool, nwalkers, "cuda", dolog10index, ypositive, temperatureArr, omegab2cut=None, docuda=False, tsize=1, gpunode=None, nnmodel_in=ChtoModelv2, params=params, method="emcee")
+
+
+if __name__ == '__main__':
+    testmain()
